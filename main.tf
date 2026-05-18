9305807cb51226880307abd5a03bfb68d3c32133
setya777 <jatityo502@gmail.com>
2026-05-18 14:38:07 +0700

    Menerapkan berkas arsitektur IaC menggunakan Terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-# 1. Menentukan cloud provider yang dituju (AWS)
+# 1. Menentukan provider cloud yang digunakan (AWS)
 provider "aws" {
-  region = "ap-southeast-1" # Wilayah Singapura (terdekat dari Indonesia)
+  region = "ap-southeast-1" # Wilayah Singapura
 }
 
 # 2. Mendefinisikan resource server virtual (EC2 Instance)
 resource "aws_instance" "uts_devops_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Contoh ID blueprint OS Ubuntu Server
-  instance_type = "t2.micro"             # Tipe spesifikasi server ekonomis (free-tier)
+  ami           = "ami-0c55b159cbfafe1f0" # Contoh ID image Ubuntu Server
+  instance_type = "t2.micro"             # Tipe spesifikasi server gratis (free-tier)
 
   tags = {
     Name        = "WebServer-UTS-DevOps"
